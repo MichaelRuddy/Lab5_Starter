89e069eb2ce628905c62c924cdcb6bdcb2070615
MichaelRuddy <62007249+MichaelRuddy@users.noreply.github.com>
2022-10-31 22:48:51 -0700

    implemented volume

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,11 +5,14 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
 }
-var selector = document.getElementById('horn-select')
+var selector = document.getElementById('horn-select');
+var images = document.querySelectorAll("img");
 var myImage = document.querySelector('img');
+var myIcon = images[1];
 var myButton = document.querySelector('button');
 var myAudioClass = document.getElementsByClassName('hidden');
 var myAudio = myAudioClass[0];
+var volumeLevel = document.getElementById("volume");
 selector.addEventListener('change', function() {
     console.log(selector.value);
     var currChoice = selector.value;
@@ -19,4 +22,19 @@ selector.addEventListener('change', function() {
   });
 myButton.addEventListener('click', function() {
   myAudio.play();
+});
+volumeLevel.addEventListener('change', function() {
+  if(volumeLevel.value == 0) {
+    myIcon.src = "assets/images/volume-level-0.svg"
+  }
+  else if(volumeLevel.value < 33) {
+    myIcon.src = "assets/images/volume-level-1.svg"
+  }
+  else if(volumeLevel.value < 67) {
+    myIcon.src = "assets/images/volume-level-2.svg"
+  }
+  else {
+    myIcon.src = "assets/images/volume-level-3.svg"
+  }  
+  myAudio.volume = volumeLevel.value / 100;
 });
