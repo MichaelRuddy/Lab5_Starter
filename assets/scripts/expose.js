86b84a690d38d28ae6265d56c1d14705e80a507f
MichaelRuddy <62007249+MichaelRuddy@users.noreply.github.com>
2022-10-31 21:22:24 -0700

    horn image

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,3 +5,12 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
 }
+selector = document.querySelector('horn-select');
+selectElement.addEventListener('change', (event) => {
+    var myImage = document.querySelector("main[src='assets/images/no-image.png']");
+    console.log(event.target.value)
+    var imageDir = "assets/images/";
+    var currChoice = event.target.value;
+    var currImage = imageDir.concat()
+    myImage.src = 'assets/images/' + 'event.target.value'
+  });
