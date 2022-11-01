aef711211ea6b5eea2609824508d31e4685bcf02
MichaelRuddy <62007249+MichaelRuddy@users.noreply.github.com>
2022-10-31 22:22:43 -0700

    fix audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,6 +7,7 @@ function init() {
 }
 var selector = document.getElementById('horn-select')
 var myImage = document.querySelector('img');
+var myButton = document.querySelector('button');
 var myAudioClass = document.getElementsByClassName('hidden');
 var myAudio = myAudioClass[0];
 selector.addEventListener('change', function() {
@@ -16,3 +17,6 @@ selector.addEventListener('change', function() {
     myImage.src = "assets/images/" + selector.value + ".svg";
     myAudio.src = "assets/audio/" + selector.value + ".mp3";
   });
+myButton.addEventListener('click', function() {
+  myAudio.play();
+});
